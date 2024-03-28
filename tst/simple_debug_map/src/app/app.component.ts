ab4837ea6335f799969a6cd10e182bf19d88fb7f
eoudejans <eoudejans@objectvision.nl>
2024-03-28 17:18:38 +0100

    Implemented simple_map, displaying maplibre demotiles example using style spec: https://demotiles.maplibre.org/style.json

--- tst/simple_debug_map/src/app/app.component.ts
+++ tst/simple_debug_map/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SimpleMapComponent } from './simple-map/simple-map.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SimpleMapComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'simple_debug_map';
 }
